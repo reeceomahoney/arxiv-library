b0122133e7818505eee94faf015270334e6410dd
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-10-26 18:16:31 +0100

    enable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
         {children}
-        {/* <Analytics /> */}
+        <Analytics />
       </body>
     </html>
   );
